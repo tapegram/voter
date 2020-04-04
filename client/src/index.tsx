9faac3cc949f20aa2fc6a607c6fb482db9a6d59c
Josh Navi <navijoshua@gmail.com>
2020-04-04 13:35:43 -0400

    fix is-plain-object

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,35 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import * as serviceWorker from './serviceWorker';
-import App from './App';
-import { Drizzle } from '@drizzle/store';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import * as serviceWorker from "./serviceWorker";
+import App from "./App";
+import { Drizzle } from "@drizzle/store";
 import { MyStringStore } from "./json";
 
 // let drizzle know what contracts we want and how to access our test blockchain
 const options = {
-    contracts: [MyStringStore],
-    web3: {
-        fallback: {
-            type: "ws",
-            url: "ws://127.0.0.1:9545",
-        },
-    },
+  contracts: [MyStringStore],
+  web3: {
+    fallback: {
+      type: "ws",
+      url: "ws://127.0.0.1:9545"
+    }
+  }
 };
 
 const props = {
-    drizzle: new Drizzle(options),
+  drizzle: new Drizzle(options)
 };
 
 ReactDOM.render(
-    <React.StrictMode>
-        <App { ...props }/>
-    </React.StrictMode>,
-    document.getElementById('root')
+  <React.StrictMode>
+    <App {...props} />
+  </React.StrictMode>,
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
-
