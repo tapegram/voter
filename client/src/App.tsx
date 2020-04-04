02b4fc54402379a09566b2f7c082b25aab7b9a80
Tavish Pegram <tapegram@gmail.com>
2020-04-04 16:11:12 -0400

    trying to get app working with drizzle types

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,57 @@
 import React from 'react';
 import './App.css';
-import {Drizzle} from "@drizzle/store";
+import {Drizzle, Store} from "@drizzle/store";
 
 type Props = {
     drizzle: Drizzle,
 };
 
-const App: React.FC<Props> = ({ drizzle }) => {
-    return (
-        <div className="App">
-            Hell World
-        </div>
-    )
+interface TDrizzle extends Drizzle {
+    store: Store,
 };
+
+type State = {
+    loading: boolean,
+    drizzle: Drizzle,
+    unsubscribe: Function,
+}
+
+class App extends React.Component<Props> {
+    state: State;
+    constructor(props: Props) {
+        super(props);
+        this.state = {
+            loading: true,
+            unsubscribe: () => ({}),
+            drizzle: props.drizzle,
+        };
+    }
+
+    componentDidMount() {
+        const { drizzle } = this.state;
+
+        // subscribe to changes in the store
+        this.setState({
+            unsubscribe: drizzle.store.subscribe(() => {
+                // every time the store updates, grab the state from drizzle
+                const drizzleState = drizzle.store.getState();
+
+                // check to see if it's ready, if so, update local component state
+                if (drizzleState.drizzleStatus.initialized) {
+                    this.setState({ loading: false, drizzleState });
+                }
+            })
+        })
+    }
+
+    componentWillUnmount() {
+        this.unsubscribe();
+    }
+
+    render() {
+        if (this.state.loading) return "Loading Drizzle...";
+        return <div className="App">Drizzle is ready</div>;
+    }
+}
 
 export default App;
